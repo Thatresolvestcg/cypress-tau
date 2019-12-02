eb9500fc9f9ce9f13bd89b1e19f6507ef4f08d72
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-02 16:24:12 +0000

    added validations for test

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -3,6 +3,11 @@
 it('should be able to add a new todo to the list', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
     cy.get('.new-todo', { timeout: 6000 }).type('Clean Room{enter}')
+    cy.get('label').should('have.text', 'Clean Room')
+    cy.get('.toggle').should('not.be.checked')
     cy.get('.toggle').click()
+    cy.get('.toggle').should('be.checked')
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
     cy.contains('Clear completed').click()
+    cy.get('.todo-list').should('not.have.descendants', 'li')
 });
