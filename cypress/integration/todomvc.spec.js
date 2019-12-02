83db7767d85b5e2a88c839d9f401e9cbff11aa34
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-02 16:09:34 +0000

    test is now a little more robust using a timeout and multiple lelements

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
-it('should navigate to the TodoMVC App', () => {
+it('should be able to add a new todo to the list', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
-})
+    cy.get('.new-todo', { timeout: 6000 }).type('Clean Room{enter}')
+    cy.get('.toggle').click()
+    cy.contains('Clear completed').click()
+});
