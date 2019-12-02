2648470803191cba7a21a40fe62462c49982142b
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-02 15:33:25 +0000

    initial test runs

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
 
 it('should navigate to the TodoMVC App', () => {
-    cy.visit('http://http://todomvc-app-for-testing.surge.sh/')
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
 })
